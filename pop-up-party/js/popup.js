83262499813c761e7baf2c08b28041c444652dc8
crooksnwhiskey <alex.chardon36@gmail.com>
2026-02-12 16:21:48 -0500

    added random box
    added a random box just so i can remember how to make a box

--- pop-up-party/js/popup.js
+++ pop-up-party/js/popup.js
@@ -13,5 +13,22 @@ function startGame() {
     title.style.display = "none";
     game.style.display = "block";
 
+    createBox()
+}
+function createBox() {
+    const box = document.createElement("div");
+
+    box.id = "test";
+
+    box.style.width = "100px"
+    box.style.height = "100px"
+    box.style.position = "absolute"
+    box.style.top = "100px"
+    box.style.left = "100px"
+    box.style.backgroundColor = "white"
+
+    const game = document.querySelector(".game-screen");
+    game.appendChild(box);
+
 
 }
